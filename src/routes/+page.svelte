78594a48c6ff2335e7a112cc9d2ba60cb4f98ec5
Pushkaraj Shinde <dev.pshkrjs@outlook.com>
2024-08-01 10:51:01 +0530

    Adding Tailwind CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="container mx-auto">
+    <div class="grid-cols-5 gap-2">
+        <div class="colspan-5 text-center pt-5 p-2">
+            <h1 class="text-5xl font-extrabold font-sans">Website Template</h1>
+            <p class="font-serif">Ready to use barebones Template Using SvelteKit and Tailwind</p>
+        </div>
+    </div>
+</main>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
+
+<svelte:head>
+    <title>Website Template</title>
+</svelte:head>
